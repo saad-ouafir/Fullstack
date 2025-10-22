b4672eb2c84f7a9fba595a737e3837da76ad99f5
Saad Ouafir <ouafirsaad2@gmail.com>
2025-10-22 21:22:51 +0100

    first mini-project was added

--- Mini_Projects/MP1_NODE_SYSTEM_LOGGER/monitor.js
+++ Mini_Projects/MP1_NODE_SYSTEM_LOGGER/monitor.js
@@ -1,12 +1,21 @@
 // monitor.js → script principal qui lit les infos système.
 import Logger from "./logger.js"; // importation du class logger
 import os from "node:os"; // importation du module `os`
+import { latestStats } from "./server.js"; // importation de la variable `latestStats` du fichier `server.js`
 
 const logger = new Logger(); // creation d'une instance de la classe `Logger`
 
 //Ecoute ce événement créer "lowMemory"
-logger.on("lowMemory",(percent)=>{//cette fct sera execute chaque fois que lévénement est émis
-  console.log(`Attension Memoire libre!  ${percent}%`);//affiche ce message d’alerte dans la console.
+logger.on("lowMemory", (percent) => {
+  //cette fct sera execute chaque fois que lévénement est émis
+  console.log(`Attension Memoire libre!  ${percent}%`); //affiche ce message d’alerte dans la console.
+});
+
+// Écoute de l'événement "lowMemory" pour afficher une alerte console
+logger.on("lowMemory", (memoryPercentage) => {
+  console.warn(
+    `Alerte ! Memoire faible !La memoire libre est ${memoryPercentage}%`
+  );
 });
 
 function logSystemRessources() {
@@ -15,8 +24,13 @@ function logSystemRessources() {
   const totalMemory = (os.totalmem() / 1024 / 1024 / 1024).toFixed(2); // garde la capacite totale de la memoire RAM en GB
   const upTime = (os.uptime() / 3600).toFixed(2); // garde en heure le nombre des heurs des le dernier demmarage de systeme
   const FreeMemoryPercentage = ((freeMemory * 100) / totalMemory).toFixed(2); // garde le pourcentage de la memoire vide en %
+
+  const now = new Date(); // creation d'une instance de la classe `Date`
+  const timestamp = now.toTimeString().split(" ")[0]; // recupere l'heure et la minute et la seconde
+
   const message = `
 -------------------------------------------------
+| [${timestamp}]
 | Free Memory       : ${freeMemory} GB          
 | Free Memory (%)   : ${FreeMemoryPercentage}%  
 | Total Memory      : ${totalMemory} GB         
@@ -25,9 +39,15 @@ function logSystemRessources() {
 `; // formattage du message pour etre pret a l'ecriture
   logger.log(message); // appel de la methode `log` pour ecrire dans le fichier log le message formatte precedement
 
+  // Mise à jour des dernières stats pour /stats
+  latestStats.freeMemory = freeMemory;
+  latestStats.totalMemory = totalMemory;
+  latestStats.freePercentage = FreeMemoryPercentage;
+  latestStats.uptimeHours = upTime;
+
   //Vérifie si la mémoire libre est inférieure à 20 % de la mémoire totale
-  if(FreeMemoryPercentage<20){
-    logger.emit("lowMemory",FreeMemoryPercentage);// Tous les écouteurs de 'lowMemory' seront exécutés
+  if (FreeMemoryPercentage < 20) {
+    logger.emit("lowMemory", FreeMemoryPercentage); // Tous les écouteurs de 'lowMemory' seront exécutés
   }
 }
 
--- Mini_Projects/MP1_NODE_SYSTEM_LOGGER/server.js
+++ Mini_Projects/MP1_NODE_SYSTEM_LOGGER/server.js
@@ -1,23 +1,34 @@
-const http=require("http");// n importiw module http bach ndir serveur
-const fs = require('fs');// n importiw module fs bach nkhdmo b fichier
-const server=http.createServer((req,res)=>{// n khlqo wahd serveur
-    if(req.url==="/"){ // ila l'utilisateur mcha l'home page   "/"
-        res.end("Bienvenue sur le Node System Logger");
-    }
-    else if(req.url==="/logs"){ // ila mcha logs
-        fs.readFile('log.txt', 'utf8', (err, data) => {//n9raw f fichier log.txt
-            if (err) {//ila makaynch fichier
-                res.statusCode = 404;
-                res.end('Fichier log.txt introuvable');
-            } else {//ila l9ah irja3 lina le contenu dialo
-                res.end(data);
-            }
-        });
-    }
-    else{// ila URL ma kaynach
-        res.statusCode=404;
-        res.end("Page non trouvée");
-    }
+import http from "node:http"; // n importiw module http bach ndir serveur
+import fs from "node:fs"; // n importiw module fs bach nkhdmo b fichier
+
+const server = http.createServer((req, res) => {
+  // n khlqo wahd serveur
+  if (req.url === "/") {
+    // ila l'utilisateur mcha l'home page   "/"
+    res.end("Bienvenue sur le Node System Logger");
+  } else if (req.url === "/logs") {
+    // ila mcha logs
+    fs.readFile("log.txt", "utf8", (err, data) => {
+      //n9raw f fichier log.txt
+      if (err) {
+        //ila makaynch fichier
+        res.statusCode = 404;
+        res.end("Fichier log.txt introuvable");
+      } else {
+        //ila l9ah irja3 lina le contenu dialo
+        res.end(data);
+      }
+    });
+  } else if (req.url === "/stats") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(latestStats, null, 2));
+  } else {
+    // ila URL ma kaynach
+    res.statusCode = 404;
+    res.end("Page non trouvée");
+  }
 });
 // serveur kay sma3 f port 3000 ay drna wahd event listen 3la had port
-server.listen(3000,()=>console.log("Serveur en écoute sur le port 3000..."));// server.js → serveur HTTP pour afficher les logs.
+server.listen(3000, () => console.log("Serveur en écoute sur le port 3000...")); // server.js → serveur HTTP pour afficher les logs.
+
+export let latestStats = {};
