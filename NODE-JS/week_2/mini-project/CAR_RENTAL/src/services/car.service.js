5ccefc6cd7dc4b403e153304e04bf6756f3e64d5
HammaAsma <161319559+HammaAsma@users.noreply.github.com>
2025-11-03 18:54:50 +0100

    Implement car service functions for CRUD operations

--- NODE-JS/week_2/mini-project/CAR_RENTAL/src/services/car.service.js
+++ NODE-JS/week_2/mini-project/CAR_RENTAL/src/services/car.service.js
@@ -1,19 +1,59 @@
-function getAllCarsService(filters) {
-  // Implémentation vide
+const path = require("path");
+const fs = require("fs");
+
+const JSON_DATA_FILE = path.join(__dirname, "../data/data.json");
+const JSON_DATA = fs.readFileSync(JSON_DATA_FILE, "utf-8");
+
+
+function getAllCarsService() {
+  return JSON_DATA;
 }
 
 function getCarByIdService(id) {
-  // Implémentation vide
+  let data = JSON.parse(JSON_DATA)
+  return data.find(c=>c.id===Number(id))
 }
 
 function createCarService(carData) {
-  // Implémentation vide
+  let data = [...JSON.parse(JSON_DATA)];
+  carData.id = data.length + 1;
+  carData.available=true;
+  carData.createdAt = new Date().toISOString();
+  carData.updatedAt=new Date().toISOString();
+  data.push(carData);
+  fs.writeFileSync(JSON_DATA_FILE, JSON.stringify(data));
+  return true;
 }
 
 function updateCarService(id, carData) {
-  // Implémentation vide
+  let data = [...JSON.parse(JSON_DATA)];
+  let index = data.findIndex((carData) => Number(carData.id === Number(id)));
+  if (index !== -1) {
+    data[index].pricePerDay = parseFloat(carData.pricePerDay);
+    data[index].available = carData.available;
+    data[index].updatedAt = new Date().toISOString();
+    fs.writeFileSync(JSON_DATA_FILE, JSON.stringify(data));
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function deleteCarService(id) {
-  // Implémentation vide
+  let data = [...JSON.parse(JSON_DATA)];
+  let new_data =
+    data.filter((carData) => Number(carData.id) !== Number(id)) || undefined;
+  if (new_data !== undefined) {
+    fs.writeFileSync(JSON_DATA_FILE, JSON.stringify(new_data));
+    return true;
+  } else {
+    return false;
+  }
+}
+module.exports={
+  getAllCarsService,
+  getCarByIdService,
+  createCarService,
+  updateCarService,
+  deleteCarService,
 }
